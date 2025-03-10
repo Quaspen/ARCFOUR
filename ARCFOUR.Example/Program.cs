59ceb784facca08a0908aa11596a3f56a6a111aa
Quaspen <217749450+quaspen@users.noreply.github.com>
2025-03-10 18:30:22 +0300

    Swap encoding to ASCII for example

--- ARCFOUR.Example/Program.cs
+++ ARCFOUR.Example/Program.cs
@@ -7,18 +7,18 @@ namespace ARCFOUR.Example
     {
         private static void Main()
         {
-            string input = "Input text!";
-            string key = "Key?";
+            string input = "Plaintext";
+            string key = "Key";
 
             // Encode
-            byte[] inputBytes = Encoding.Unicode.GetBytes(input);
-            byte[] keyBytes = Encoding.Unicode.GetBytes(key);
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] keyBytes = Encoding.ASCII.GetBytes(key);
             byte[] cipherBytes = RC4.Apply(inputBytes, keyBytes);
             Console.WriteLine($"Encoded text (in Hexadecimal form): \"{Convert.ToHexString(cipherBytes)}\"");
 
             // Decode
             byte[] outputBytes = RC4.Apply(cipherBytes, keyBytes);
-            Console.WriteLine($"Decoded source text: \"{Encoding.Unicode.GetString(outputBytes)}\"");
+            Console.WriteLine($"Decoded source text: \"{Encoding.ASCII.GetString(outputBytes)}\"");
         }
     }
 }
