d74ae96f72665328db18d89f68f30bacabe3d77f
Quaspen <217749450+quaspen@users.noreply.github.com>
2025-03-10 16:56:12 +0300

    Добавьте файлы проекта.

---
+++ ARCFOUR.Example/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using Maniekko.Cryptography;
+
+namespace ARCFOUR.Example
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            string input = "Input text!";
+            string key = "Key?";
+
+            // Encode
+            byte[] inputBytes = Encoding.Unicode.GetBytes(input);
+            byte[] keyBytes = Encoding.Unicode.GetBytes(key);
+            byte[] cipherBytes = RC4.Apply(inputBytes, keyBytes);
+            Console.WriteLine($"Encoded text (in Hexadecimal form): \"{Convert.ToHexString(cipherBytes)}\"");
+
+            // Decode
+            byte[] outputBytes = RC4.Apply(cipherBytes, keyBytes);
+            Console.WriteLine($"Decoded source text: \"{Encoding.Unicode.GetString(outputBytes)}\"");
+        }
+    }
+}
---
+++ ARCFOUR/Class1.cs
@@ -0,0 +1,46 @@
+﻿namespace Maniekko.Cryptography
+{
+    public static class RC4
+    {
+        /// <summary>
+        /// Apply RC4 encoding to a input array of bytes, with a specified key. <para/>
+        /// RC4 is symmetrical, so the same method is used for both data encoding and decoding.
+        /// </summary>
+        /// <param name="input">Array of bytes to be encoded.</param>
+        /// <param name="key">Key for encoding.</param>
+        /// <returns>Encypted array of bytes.</returns>
+        public static byte[] Apply(byte[] input, byte[] key)
+        {
+            byte i, j;
+
+            // State is first initialized with a identity permutation of bytes
+            byte[] State = new byte[256];
+            for (i = 0; i < 255; i++)
+                State[i] = i;
+
+            // State is then processed with PRGA with keys bytes mixed in.
+            j = 0;
+            for (i = 0; i < 255; i++)
+            {
+                j = (byte)((j + State[i] + key[i % key.Length]) % 256);
+                (State[i], State[j]) = (State[j], State[i]); // Swap State[i] and State[j]
+            }
+
+            // For each byte of input, PRGA is used for getting the ciphertext of that byte.
+            // RC4 is symmetrical, meaning applying this algorithm 
+            // for ciphertext will return the initial text
+            byte[] result = new byte[input.Length];
+            for(long pos=0; pos < input.LongLength; pos++)
+            {
+                i = (byte)((i + 1) % 256);
+                j = (byte)((j + State[i]) % 256);
+                (State[i], State[j]) = (State[j], State[i]); // Swap State[i] and State[j]
+                byte t = (byte)((State[i] + State[j]) % 256);
+                byte k = State[t];
+                result[pos] = (byte)(input[pos] ^ k);
+            }
+
+            return result;
+        }
+    }
+}
